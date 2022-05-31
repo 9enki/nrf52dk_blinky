264f67e741e9d2c86f1137b8bf16c6e9c0509d7e
ikneg <0930019@gmail.com>
2022-05-31 12:21:11 +0900

    first commit

---
+++ build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD nrf52dk_nrf52810 CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE C:/Users/gnk/nordic/blinky/prj.conf CACHE INTERNAL "NCS child image controlled")
+set(CONFIG_DEBUG_OPTIMIZATIONS y CACHE INTERNAL "NCS child image controlled")
+set(CONFIG_DEBUG_THREAD_INFO y CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE  CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION NONE CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON c:\Users\gnk\ncs\v1.9.1\toolchain\opt\bin\python.exe CACHE INTERNAL "NCS child image controlled")
---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,12 @@
+empty_file.o: C:/Users/gnk/ncs/v1.9.1/zephyr/misc/empty_file.c \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/boards/arm/nrf52dk_nrf52810/nrf52dk_nrf52810.dts \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/dts/arm/nordic/nrf52810_qfaa.dtsi \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/dts/common/mem.h \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/dts/arm/nordic/nrf52810.dtsi \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/dts/arm/armv7-m.dtsi \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/dts/common/skeleton.dtsi \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/include/dt-bindings/i2c/i2c.h \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/include/dt-bindings/gpio/gpio.h \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/include/dt-bindings/pinctrl/nrf-pinctrl.h \
+ C:/Users/gnk/ncs/v1.9.1/zephyr/dts/arm/nordic/override.dtsi
